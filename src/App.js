7a23d4ba46bc2435584920b6f2170a733d132b8c
Anri Liy <amedamachip713@gmail.com>
2021-10-05 19:07:16 -0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import Todo from './components/Todo';
+import { Route, Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React' />
-      <Todo text='Master React' />
-      <Todo text='Create Project' />
-    </div>
-  );
+  return (<div>
+    <MainNavigation />
+    <Switch>
+      <Route path='/' exact>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+    </Switch>
+  </div>);
 }
 
 export default App;

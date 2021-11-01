44c851d65db33dd64a895bbe59a58b4834248f4e
Anri Liy <amedamachip713@gmail.com>
2021-10-31 17:03:46 -0700

    Context3

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { FavoritesContextProvider } from './store/favorittes-context';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>,
+  document.getElementById('root')
+);
--- src/store/favorittes-context.js
+++ src/store/favorittes-context.js
@@ -2,19 +2,45 @@ import { createContext, useState } from "react";
 
 const FavoritesContext = createContext({
   favorites: [],
-  totalFavorites: 0
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFovorite: (meetupId) => {}
 });
 
-function FavoritesContextProvider(props) {
+export function FavoritesContextProvider(props) {
 
   const [userFavorites, setUserFavorites] = useState([]);
+
+  const addFavoriteHandler = (favoriteMeetup) => {
+    // setUserFavorites(userFavorites.concat(favoriteMeetup));
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  };
+
+  const removeFavoriteHandler = (meetupId) => {
+    setUserFavorites(prevUserFavorites => {
+      return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+    });
+  };
+
+  const itemIsFavoriteHandler = (meetupId) => {
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  };
+
 
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFovorite: itemIsFavoriteHandler
   };
 
   return <FavoritesContext.Provider value={context}>
     {props.children}
   </FavoritesContext.Provider>
 }
+
+export default FavoritesContext;
